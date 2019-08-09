6900951a1696a89dd921165aea9f4a8f205fea14
Abram Situmorang <abram.perdanaputra@gmail.com>
2019-08-09 17:32:53 +0700

    Initial Commit

---
+++ TestBazel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestBazel
+//
+//  Created by Abram Situmorang on 09/08/19.
+//  Copyright © 2019 Tokopedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
